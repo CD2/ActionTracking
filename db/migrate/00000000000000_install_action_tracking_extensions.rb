7985266be1e6328fa2bae943890e99f6f18572bf
James Page <james.page@cd2solutions.co.uk>
2017-09-26 08:50:05 +0100

    remove empty migration

--- db/migrate/00000000000000_install_action_tracking_extensions.rb
+++
@@ -1,9 +0,0 @@
-class InstallActionTrackingExtensions < ActiveRecord::Migration[5.1]
-  def up
-    # execute 'CREATE EXTENSION IF NOT EXISTS ???;'
-  end
-
-  def down
-    # execute 'DROP EXTENSION IF EXISTS ???;'
-  end
-end
